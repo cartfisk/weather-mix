3af797643a7c30f7ea72f95bb4ee708a75333bfe
Jesse <jmeza081@gmail.com>
2017-03-12 17:33:57 -0500

    Added API parsing of weather conditions and updating UI on enter

--- weather-mix/assets/scripts/App.js
+++ weather-mix/assets/scripts/App.js
@@ -3,6 +3,8 @@ var appId = "b84bed13d654df469398701da69d1071";
 // baseurl for zipcode based API call to openweathermap
 var baseUrl = "http://api.openweathermap.org/data/2.5/weather?";
 
+var weatherIconBaseUrl = "http://openweathermap.org/img/w/";
+
 // HTML elements from index.html
 var zipcodeInput = document.getElementById('zipcode');
 var submitButton = document.getElementById('zip-submit');
@@ -12,11 +14,18 @@ var countryCode = "us";
 // weather code returned from openweathermap
 var weatherCode;
 
+//Get the current Condition Icon Code.
+var iconCode;
+
+//Get weather Condition
+var condition;
+var temp;
+
 // called on keyup event in zip input field, constructs
 function updateZip() {
   if (zipcodeInput.value.length >= 5) {
     zipCode = "zip=" + zipcodeInput.value + ",us&APPID=" + appId;
-    var url = baseUrl + zipCode;
+    var url = baseUrl + zipCode + "&units=imperial";
     console.log(zipCode);
     sendRequest(url);
   }
@@ -28,9 +37,22 @@ function sendRequest(url){
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
       var data = JSON.parse(xmlhttp.responseText);
 	    var weather = {};
-	    weather.code = data.weather[0].id;
-      weatherCode = weather.code;
-      console.log(weatherCode);
+	weather.code = data.weather[0].id;
+	weatherCode = weather.code;
+
+	condition = data.weather[0].main;
+	temp = data.main["temp"];
+	iconCode = data.weather[0].icon;
+	
+	console.log(weatherCode);
+	console.log("Current Condition: " + condition);
+	console.log("Current Temp: " + temp);
+	console.log("Icon Code: " + iconCode);
+
+	setWeatherIcon();
+	setCurTempText();
+	setConditionText();
+	
     }
   };
 
@@ -39,3 +61,17 @@ function sendRequest(url){
 }
 
 zipcodeInput.addEventListener('keyup', updateZip, false);
+
+
+//Sets the current weather icon for the UI
+var setWeatherIcon = function(){
+    $("#weather-icon").attr("src", weatherIconBaseUrl + iconCode + ".png");
+};
+
+var setCurTempText = function(){
+    $("#temperature").text(temp + "°");
+}
+
+var setConditionText = function(){
+    $("#condition").text(condition);
+}
